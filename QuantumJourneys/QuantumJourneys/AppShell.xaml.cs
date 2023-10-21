c324d20971fa866fa18f85eb44b5ac47b47d167d
wizi4884 <68853051+AndrewChebotarev@users.noreply.github.com>
2023-10-21 16:29:44 +0300

    Create proj

---
+++ QuantumJourneys/QuantumJourneys/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace QuantumJourneys;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ QuantumJourneys/QuantumJourneys/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace QuantumJourneys;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
